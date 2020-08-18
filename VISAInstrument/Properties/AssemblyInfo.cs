5208b3cd6d0b728f33f5b8da8a732de3e786a1ea
CNXY <cnxy@cnxy.me>
2020-08-18 09:45:35 +0800

    版本更新至1.2.0.4

--- VISAInstrument/Properties/AssemblyInfo.cs
+++ VISAInstrument/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("VISAInstrument")]
-[assembly: AssemblyDescription("http://www.cnc6.cn")]
+[assembly: AssemblyDescription("http://cnxy.me")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("by CNXY")]
 [assembly: AssemblyProduct("VISAInstrument")]
-[assembly: AssemblyCopyright("Copyright © CNXY  2017")]
+[assembly: AssemblyCopyright("Copyright © CNXY  2020")]
 [assembly: AssemblyTrademark("CNXY")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.3")]
-[assembly: AssemblyFileVersion("1.2.0.3")]
+[assembly: AssemblyVersion("1.2.0.4")]
+[assembly: AssemblyFileVersion("1.2.0.4")]
